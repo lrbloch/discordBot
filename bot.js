cc70bedcb518c9267439000f0f4992070e182c2c
Laura Bloch <lbloch220@gmail.com>
2020-08-04 13:19:13 -0400

    uses node-emoji to parse emoji reactions

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const emojiTranslate = require('node-emoji');   //NEW (npm -i --save node-emoji)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -10,6 +11,11 @@ client.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('pong');
     }
+});
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    var emoji = messageReaction.emoji;
+    console.log(emojiTranslate.which(emoji.name));  //NEW
 });
 
 client.login(auth.token);
